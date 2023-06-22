dd3319d4f1608feaeb409494b4ce511c172ce855
Lou <lou.yiw@gmail.com>
2023-06-23 01:27:20 +0800

    feat(general): muti-language

---
+++ src/languages.ts
@@ -0,0 +1,18 @@
+/** en, cn */
+
+const Lang = {
+  authentication: {
+    logIn: { eng: 'Log In', cn: '登入' },
+    signIn: { eng: 'Sign In', cn: '註冊' },
+    logInSuccess: { eng: 'Successfully logged in', cn: '登入成功' },
+    signInSuccess: { eng: 'Successfully signed In', cn: '註冊成功' },
+    logInFailure: { eng: 'Failed to log in', cn: '登入失敗' },
+    signInFailuire: { eng: 'Failed to sign In', cn: '註冊失敗' },
+  },
+  errorMsg: {
+    minWord: { eng: ['At least ', ' words.'], cn: ['至少', '字'] },
+    required: { eng: 'Required.', cn: '必填' },
+  },
+};
+
+export default Lang;
